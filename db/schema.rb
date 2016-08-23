1a535ecee5ba9d0ffd7cbb937c51a5972f89bab7
Aleksandr Kunin <ab@ipkunin.ru>
2016-08-23 11:51:21 +0300

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814115626) do
+ActiveRecord::Schema.define(version: 20160821221118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -366,7 +366,6 @@ ActiveRecord::Schema.define(version: 20160814115626) do
     t.float    "result",                 default: 0.0
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "profile_id"
     t.float    "highest_speed",          default: 0.0
     t.float    "highest_gr",             default: 0.0
   end
